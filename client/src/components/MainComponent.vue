6afa6fc859f1e4c416ab12eb90b429793b82ef75
Do Anh Thong <thong.da@live.com>
2023-04-09 13:34:41 +0700

    Add Loading Indicator

--- client/src/components/MainComponent.vue
+++ client/src/components/MainComponent.vue
@@ -1,45 +1,58 @@
 <template>
     <div id="app">
-      <LeftPanel @generateChart="generateChart" />
-      <RightPanel :chartData="chartData" />
+        <LeftPanel @generateChart="generateChart" />
+        <div v-if="isLoading" class="loading-indicator">
+            Loading...
+        </div>
+        <RightPanel :chartData="chartData" />
     </div>
-  </template>
+</template>
   
-  <script>
-  import LeftPanel from './LeftPanel.vue';
-  import RightPanel from './RightPanel.vue';
-  
-  export default {
+<script>
+import LeftPanel from './LeftPanel.vue';
+import RightPanel from './RightPanel.vue';
+
+export default {
     name: 'App',
     components: { LeftPanel, RightPanel },
     data() {
-      return {
-        chartData: null,
-      };
+        return {
+            chartData: null,
+            isLoading: false,
+        };
     },
     methods: {
-      async generateChart(prompt) {
-        try {
-            
-            // Call the backend API to generate the VegaLite chart data
-            const response = await fetch('http://localhost:3000/api/generate-chart', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ prompt }),
-            });
-            
-
-            // Check if the response is ok (status code in the range 200-299)
-            if (!response.ok) {
-            throw new Error(`HTTP error ${response.status}`);
+        async generateChart(prompt) {
+            if (!prompt || prompt.trim() === "") {
+                console.error("Prompt is empty or invalid");
+                return;
             }
 
-            this.chartData = await response.json();
-            console.log('Chart data:', this.chartData);
-        } catch (error) {
-            console.error('Error fetching chart data:', error);
-        }
-      },
+            this.isLoading = true;
+
+            try {
+
+                // Call the backend API to generate the VegaLite chart data
+                const response = await fetch('http://localhost:3000/api/generate-chart', {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({ prompt }),
+                });
+
+
+                // Check if the response is ok (status code in the range 200-299)
+                if (!response.ok) {
+                    throw new Error(`HTTP error ${response.status}`);
+                }
+
+                this.chartData = await response.json();
+                console.log('Chart data:', this.chartData);
+            } catch (error) {
+                console.error('Error fetching chart data:', error);
+            }
+
+            this.isLoading = false;
+        },
     },
-  };
-  </script>
+};
+</script>
